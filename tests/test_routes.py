95eab0f80393f38f3df21006761dfb947b07c72e
Michael Black <michaeldblack@example.com>
2026-09-11 00:50:19 -0400

    Fix lint errors in test_routes

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -183,9 +183,9 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_method_not_allowed(self):
-            """It should not allow an illegal method call"""
-            resp = self.client.delete(BASE_URL)
-            self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+        """It should not allow an illegal method call"""
+        resp = self.client.delete(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def test_security_headers(self):
         """It should return security headers"""
@@ -206,3 +206,4 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         # Check for the CORS header
         self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
+        
